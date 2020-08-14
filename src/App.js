f91bb1c9fefb31cfb69a33893967e332dbab9a89
joye <zhengyumeng@shouqianba.com>
2020-08-14 10:50:16 +0800

    fsd

--- src/App.js
+++ src/App.js
@@ -6,13 +6,28 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img
+          src={
+            logo
+          }
+          className="App-logo"
+          alt="logo"
+        />
         <p>
           Edit
-          {'         '}
-          <code>src/App.js</code>
-          {'   '}
-          and save to reload.
+          {
+            '  '
+          }
+          <code>
+            src/App.js
+          </code>
+          {
+            '  '
+          }
+          and
+          save
+          to
+          reload.
         </p>
         <a
           className="App-link"
@@ -20,7 +35,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn
+          React
         </a>
       </header>
     </div>
